c0812ba04bae68e83c665327e63378f13df09e10
Brandon <brandon.thio@yahoo.com.sg>
2022-05-31 17:14:28 +0800

    Add ability to upload both from  files/gallery

--- lib/widgets/upload_file_button.dart
+++ lib/widgets/upload_file_button.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:leopard_demo/redux_/rootStore.dart';
@@ -15,17 +16,38 @@ class UploadFileButton extends StatelessWidget {
           store.state.untitled.pickFile,
           store.state.untitled.removeSelectedFile),
       builder: (_, viewModel) {
-        return Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ElevatedButton(
-            onPressed: viewModel.file == null
-                ? viewModel.pickFile
-                : viewModel.removeFile,
-            child: viewModel.file == null
-                ? Text('Upload Audio')
-                : Text('Remove Audio'),
-          ),
-        );
+        return CupertinoContextMenu(
+            actions: <Widget>[
+              CupertinoContextMenuAction(
+                trailingIcon: CupertinoIcons.folder_open,
+                child: const Text('Choose Files'),
+                onPressed: () {
+                  viewModel.pickFile(fromGallery: false);
+                  Navigator.of(context).pop();
+                },
+              ),
+              CupertinoContextMenuAction(
+                trailingIcon: CupertinoIcons.photo_on_rectangle,
+                child: const Text('Photo Library'),
+                onPressed: () {
+                  viewModel.pickFile(fromGallery: true);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (viewModel.file != null) {
+                    viewModel.removeFile();
+                  }
+                },
+                child: viewModel.file == null
+                    ? Text('Upload Audio')
+                    : Text('Remove Audio'),
+              ),
+            ));
       },
     );
   }
@@ -33,7 +55,7 @@ class UploadFileButton extends StatelessWidget {
 
 class UploadFileButtonVM {
   File? file;
-  void Function() pickFile;
+  void Function({bool fromGallery}) pickFile;
   void Function() removeFile;
   UploadFileButtonVM(this.file, this.pickFile, this.removeFile);
 }

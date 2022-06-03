5722fe23ef98d1ccc463b4203caa391d34bc894c
Brandon <brandon.thio@yahoo.com.sg>
2022-06-03 15:41:01 +0800

    Add restart button

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:settings_ui/settings_ui.dart';
+
+import '../redux_/rootStore.dart';
+import '../redux_/untitled.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -20,69 +22,82 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Settings')),
-      body: Container(
-        child: SettingsList(
-          sections: [
-            SettingsSection(
-              title: Text('Set Up'),
-              tiles: <SettingsTile>[
-                SettingsTile(
-                  trailing: Expanded(
-                    child: TextField(
-                      controller: TextEditingController(text: 'Hello'),
-                      decoration: InputDecoration.collapsed(hintText: ''),
-                      textAlign: TextAlign.end,
-                      autocorrect: false,
+    return StoreProvider(
+      store: store,
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Settings')),
+        body: Container(
+          child: SettingsList(
+            sections: [
+              SettingsSection(
+                title: Text('Set Up'),
+                tiles: <SettingsTile>[
+                  SettingsTile(
+                    trailing: Expanded(
+                      child: TextField(
+                        controller: TextEditingController(text: 'Hello'),
+                        decoration: InputDecoration.collapsed(hintText: ''),
+                        textAlign: TextAlign.end,
+                        autocorrect: false,
+                      ),
+                    ),
+                    leading: Icon(Icons.key),
+                    title: Text('Access Key'),
+                  ),
+                ],
+              ),
+              SettingsSection(
+                title: Text(
+                  'Key Phrases',
+                ),
+                tiles: <SettingsTile>[
+                  SettingsTile(
+                    title: Text(
+                      'FULL STOP',
+                    ),
+                    value: Text(
+                      '.',
+                      style: TextStyle(color: Color.fromRGBO(142, 142, 147, 1)),
                     ),
                   ),
-                  leading: Icon(Icons.key),
-                  title: Text('Access Key'),
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: Text(
-                'Key Phrases',
+                  SettingsTile(
+                    title: Text('PERIOD'),
+                    value: UNEDITABLE_TEXT('.'),
+                  ),
+                  SettingsTile(
+                    title: Text('COMMA'),
+                    value: UNEDITABLE_TEXT(','),
+                  ),
+                  SettingsTile(
+                    title: Text('NEW LINE'),
+                    value: UNEDITABLE_TEXT(r'\n\n'),
+                  ),
+                  SettingsTile(
+                    title: Text('OPEN BRACKET'),
+                    value: UNEDITABLE_TEXT(r'('),
+                  ),
+                  SettingsTile(
+                    title: Text('FINISH BRACKET'),
+                    value: UNEDITABLE_TEXT(r')'),
+                  ),
+                  SettingsTile(
+                    title: Text('MAKE POINT'),
+                    value: UNEDITABLE_TEXT(r'\n\n- '),
+                  ),
+                ],
               ),
-              tiles: <SettingsTile>[
+              SettingsSection(tiles: [
                 SettingsTile(
-                  title: Text(
-                    'FULL STOP',
-                  ),
-                  value: Text(
-                    '.',
-                    style: TextStyle(color: Color.fromRGBO(142, 142, 147, 1)),
-                  ),
-                ),
-                SettingsTile(
-                  title: Text('PERIOD'),
-                  value: UNEDITABLE_TEXT('.'),
-                ),
-                SettingsTile(
-                  title: Text('COMMA'),
-                  value: UNEDITABLE_TEXT(','),
-                ),
-                SettingsTile(
-                  title: Text('NEW LINE'),
-                  value: UNEDITABLE_TEXT(r'\n\n'),
-                ),
-                SettingsTile(
-                  title: Text('OPEN BRACKET'),
-                  value: UNEDITABLE_TEXT(r'('),
-                ),
-                SettingsTile(
-                  title: Text('FINISH BRACKET'),
-                  value: UNEDITABLE_TEXT(r')'),
-                ),
-                SettingsTile(
-                  title: Text('MAKE POINT'),
-                  value: UNEDITABLE_TEXT(r'\n\n- '),
-                ),
-              ],
-            ),
-          ],
+                  title: Text('Restart'),
+                  trailing: IconButton(
+                      onPressed: () {
+                        store.dispatch(UntitledState.initLeopard);
+                      },
+                      icon: Icon(Icons.refresh_rounded)),
+                )
+              ])
+            ],
+          ),
         ),
       ),
     );
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-final String accessKey = '';
-// '3oGp4ivh8O72YYazPiNsioX6z4eM78P0yReachZZPqmwm4wjkxuWUg=='; // AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)
+// final String accessKey = '';
+final String accessKey =
+    '3oGp4ivh8O72YYazPiNsioX6z4eM78P0yReachZZPqmwm4wjkxuWUg=='; // AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)
 final int maxRecordingLengthSecs = 60000; // 1 Hour
 final String BEGIN_FLAG = '⟨';
 final String TERMINATING_FLAG = '⟩';

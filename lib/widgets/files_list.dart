ef99294e73e56b6c07a2295f08bea285c597001b
Brandon <brandon.thio@yahoo.com.sg>
2022-06-07 14:44:53 +0800

    add pull to refresh

--- lib/widgets/files_list.dart
+++ lib/widgets/files_list.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:Minutes/utils/extensions.dart';
 import 'package:Minutes/utils/transcriptClasses.dart';
-import 'package:Minutes/widgets/just_audio_player.dart';
 import 'package:Minutes/widgets/secondary_icon_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +12,9 @@ import '../screens/transcript_screen.dart';
 
 class FilesList extends StatefulWidget {
   final List<File> files;
-  const FilesList({Key? key, required this.files}) : super(key: key);
+  final Future<void> Function() onRefresh;
+  const FilesList({Key? key, required this.files, required this.onRefresh})
+      : super(key: key);
 
   @override
   State<FilesList> createState() => _FilesListState();
@@ -44,45 +45,52 @@ class _FilesListState extends State<FilesList> {
               if (snapshot.hasError) {
                 return Text('Error: ${snapshot.error}');
               } else {
-                return ListView(
-                    children: snapshot.data!
-                        .map(
-                          (transcript) => Card(
-                            elevation: 5,
-                            margin: const EdgeInsets.all(10.0),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: SizedBox(
-                                  height: 100,
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        transcript.audio.getFileName(),
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w600),
-                                      ),
-                                      SecondaryIconButton(
-                                          onPress: () {
-                                            store.dispatch(
-                                                loadTranscript(transcript));
-                                            JustAudioPlayerWidgetState.init(
-                                                transcript.audio);
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        TranscriptScreen()));
-                                          },
-                                          icon: Icon(CupertinoIcons
-                                              .arrow_right_circle_fill),
-                                          margin: EdgeInsets.zero)
-                                    ],
-                                  )),
-                            ),
-                          ),
-                        )
-                        .toList());
+                return RefreshIndicator(
+                  onRefresh: () => widget.onRefresh(),
+                  child: ListView(
+                      children: snapshot.data!.isEmpty
+                          ? [
+                              Text(
+                                  'No Transcriptions Found. Click the + Button to start transcribing!')
+                            ]
+                          : snapshot.data!
+                              .map(
+                                (transcript) => Card(
+                                  elevation: 5,
+                                  margin: const EdgeInsets.all(10.0),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: SizedBox(
+                                        height: 100,
+                                        child: Column(
+                                          children: [
+                                            Text(
+                                              transcript.audio.getFileName(),
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                            SecondaryIconButton(
+                                                onPress: () {
+                                                  store.dispatch(loadTranscript(
+                                                      transcript));
+
+                                                  Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              TranscriptScreen()));
+                                                },
+                                                icon: Icon(CupertinoIcons
+                                                    .arrow_right_circle_fill),
+                                                margin: EdgeInsets.zero)
+                                          ],
+                                        )),
+                                  ),
+                                ),
+                              )
+                              .toList()),
+                );
               }
           }
         });

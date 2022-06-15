388fdccd3eec6a08276b8a08890b3a1e94d578b8
Brandon <brandon.thio@yahoo.com.sg>
2022-06-15 16:50:49 +0800

    Initial commit for CoreML AlbertPunctuator

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,35 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let punctuatorChannel = FlutterMethodChannel(name: "minutes/punctuator",
+                                                binaryMessenger: controller.binaryMessenger)
+      punctuatorChannel.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        // This method is invoked on the UI thread.
+          switch call.method {
+          case "punctuateText":
+              guard let args = call.arguments as? [String: String] else { return }
+              let text = args["text"]!
+              
+              self?.punctuateText(text: text, result: result)
+          default:
+              result(FlutterMethodNotImplemented)
+          }
+      })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    private func punctuateText(text: String, result: FlutterResult) {
+        do {
+            let myModel = try PunctuatorModel()
+            let punctuator = AlbertPunctuator(model: myModel)
+            let (softMaxScores, words, mask) = punctuator.punctuate(text: text)
+//            print(softMaxScores, words, mask)
+            result(["scores" : softMaxScores, "words" : words, "mask" : mask])
+        } catch let error {
+            print(error.localizedDescription)
+            result(FlutterError(code: "An error occured", message: error.localizedDescription, details: nil))
+        }
+    }
 }

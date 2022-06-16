32fd7be77d3fbcb4b2252c7e791c89ba930da4e2
Brandon <brandon.thio@yahoo.com.sg>
2022-06-16 13:54:56 +0800

    Change text color to white

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 final Color bgColor = Colors.black;
 final Color accentColor = Color.fromARGB(255, 13, 13, 13);
-final Color textColor = almostWhite;
+final Color textColor = Colors.white;
 final Color almostWhite = Color.fromARGB(210, 255, 255, 255);
 final Color secondaryTextColor = Colors.white;
 final Color focusedTextColor = Colors.white;
